cb86321bac8b0a17b5ef1652b5a6797f867a01fd
Emilien Kenler <hello@emilienkenler.com>
2012-11-10 17:30:34 +0100

    create chicken
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- create_chicken.sh
+++ create_chicken.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Add User
-MC_USER=$1
+MC_USER=srv$1
+PORT=$((25500+$1))
 
 useradd -g minecraft -s /bin/bash -m ${MC_USER}
 chmod -R go-rwx /home/${MC_USER}
 wget http://dl.hostyourcreeper.com/craftbukkit/craftbukkit.jar \
   -O /home/${MC_USER}/minecraft/craftbukkit.jar >/dev/null 2>&1
 chown ${MC_USER}:minecraft /home/${MC_USER}/minecraft/craftbukkit.jar
+sed -i 's/PORT=25565/PORT=$PORT/' /home/${MC_USER}/minecraft.sh
 echo "Server installed"
