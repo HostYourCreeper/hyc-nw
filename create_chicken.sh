a19fba74a35f2bef9461a18abde0d790b3084a97
Emilien Kenler <hello@emilienkenler.com>
2012-11-11 15:27:53 +0100

    chicken install
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- create_chicken.sh
+++ create_chicken.sh
@@ -8,7 +8,7 @@ useradd -g minecraft -s /bin/bash -m ${MC_USER}
 chmod -R go-rwx /home/${MC_USER}
 wget http://dl.hostyourcreeper.com/craftbukkit/craftbukkit.jar \
   -O /home/${MC_USER}/minecraft/craftbukkit.jar >/dev/null 2>&1
-chown ${MC_USER}:minecraft /home/${MC_USER}/minecraft/craftbukkit.jar
+chown -R ${MC_USER}:minecraft /home/${MC_USER}
 sed -i "s/PORT=25565/PORT=$PORT/" /home/${MC_USER}/minecraft.sh
-su ${MC_USER} -c '/home/${MC_USER}/minecraft.sh start'
+su ${MC_USER} -c '$HOME/minecraft.sh start'
 echo "Server installed"
