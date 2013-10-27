06e3f1820a6505c1e0c52b2e4cb1e36751f4b8a0
HYC9 <milk@hostyourcreeper.com>
2013-10-27 18:14:36 +0100

    Secu

--- create_chicken.sh
+++ create_chicken.sh
@@ -6,11 +6,15 @@ PORT=$((25500+$1))
 
 useradd -g minecraft -s /bin/bash -m ${MC_USER}
 chmod -R go-rwx /home/${MC_USER}
-#chmod a-w /home/${MC_USER}
 mkdir -p /home/${MC_USER}/minecraft
 wget http://dl.hostyourcreeper.com/craftbukkit/craftbukkit.jar \
   -O /home/${MC_USER}/minecraft/craftbukkit.jar >/dev/null 2>&1
 chown -R ${MC_USER}:minecraft /home/${MC_USER}
+chown root:minecraft /home/${MC_USER}/minecraft.sh
+chown root:minecraft /home/${MC_USER}
+chmod 1750 /home/${MC_USER}
+chmod 750 /home/${MC_USER}/minecraft.sh
 sed -i "s/PORT=25565/PORT=$PORT/" /home/${MC_USER}/minecraft.sh
 su ${MC_USER} -c '$HOME/minecraft.sh start'
 echo "Server installed"
+
